da6c4bb5b5d4f589fbb97e78c0c49a56d0739739
Tharusha Dilshan <tharusha22589@gmail.com>
2024-01-27 13:02:59 +0530

    portal page

---
+++ lib/ar_portal_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ArPortalPage extends StatelessWidget{
+  const  ArPortalPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return Scaffold()
+  }
+}
