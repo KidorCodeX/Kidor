8f55a653c5aec7e7f854f2bbd851c258d5020c70
Amadee Ranasinghe <amadeeranasinghe@gmail.com>
2024-01-28 14:54:55 +0530

    Create home_pg.dart

---
+++ lib/home_pg.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Stateless Widget'),
+      ),
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyStatelessWidget(),
+    );
+  }
+}
