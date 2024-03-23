7464351a5873201212a421c8eb35796222810594
Devhan Dodampahala <devhandod@gmail.com>
2024-03-24 02:48:17 +0530

    commented main

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-//import 'package:my_first_app/biology_page.dart';
-//import 'package:my_first_app/login_page.dart';
-//import 'package:my_first_app/mcq_page.dart';
-//import 'package:my_first_app/walk.dart';
-//import 'package:my_first_app/home_page.dart';
-//import 'package:my_first_app/lessons_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-//import 'package:my_first_app/firebase_options.dart';
-//import 'package:my_first_app/splash_screen.dart';
-//import 'package:my_first_app/welcome_page.dart';
+// import 'package:flutter/material.dart';
+// //import 'package:my_first_app/biology_page.dart';
+// //import 'package:my_first_app/login_page.dart';
+// //import 'package:my_first_app/mcq_page.dart';
+// //import 'package:my_first_app/walk.dart';
+// //import 'package:my_first_app/home_page.dart';
+// //import 'package:my_first_app/lessons_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// //import 'package:my_first_app/firebase_options.dart';
+// //import 'package:my_first_app/splash_screen.dart';
+// //import 'package:my_first_app/welcome_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const Myapp());
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(const Myapp());
+// }
 
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
+// class Myapp extends StatelessWidget {
+//   const Myapp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "App bar",
-      debugShowCheckedModeBanner: false,
-      //home: WelcomePage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "App bar",
+//       debugShowCheckedModeBanner: false,
+//       //home: WelcomePage(),
+//     );
+//   }
+// }
