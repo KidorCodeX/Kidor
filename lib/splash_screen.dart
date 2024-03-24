c7e68bb14720ca26e7d3750bd90ad06539e80f15
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 16:45:58 +0530

    splash screen full page

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -20,4 +20,24 @@ class SplashScreen extends StatelessWidget {
             splashTransition: SplashTransition.scaleTransition,
             duration: 5000,
           ),
-          
+          const Positioned(
+            bottom: 20.0,
+            left: 0,
+            right: 0,
+            child: Text(
+              'Kidor',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.black,
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
