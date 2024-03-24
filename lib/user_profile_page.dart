c3e51559cc072970edbf5f9e5cd15a1cf6ba4ff8
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 16:56:42 +0530

    User profile full page added

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -344,3 +344,53 @@ class UserProfile extends StatelessWidget {
                                 ),
                               ),
                               ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 8),
+                                child: SizedBox(
+                                  width: 91,
+                                  height: 95,
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "20%",
+                                        style: 
+                                        TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold
+                                        )
+                                      ),
+                                      Text(
+                                        "Quizzes",
+                                        style: 
+                                        TextStyle(
+                                          fontSize: 15
+                                        )
+                                      ),
+                                      
+                                      
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                     ),
+                    
+                  ],
+                ),
+              )
+            ),
+
+          ],
+        )
+      
+      
+      
+
+
+    );
+    
+  }
+}
