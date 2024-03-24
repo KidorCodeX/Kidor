24c6ed0ad71735e576cbb0ee0910c4e43cca94cd
Amadee Ranasinghe <amadeeranasinghe@gmail.com>
2024-03-24 17:21:33 +0530

    email verification and sign out

--- lib/firebase_auth_methods.dart
+++ lib/firebase_auth_methods.dart
@@ -1,22 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
-class FirebaseAuthMethods{
+class FirebaseAuthMethods {
   final FirebaseAuth _auth;
   FirebaseAuthMethods(this._auth);
 
   User get user => _auth.currentUser!;
   //Email sign up
-  Future<void>  signUpWithEmail({
+  Future<void> signUpWithEmail({
     required String email,
-    required String  password,
+    required String password,
     required BuildContext context,
-  }) async{
-    try{
+  }) async {
+    try {
       await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
+        email: email,
+        password: password,
       );
       await sendEmailVerification(context);
     } on FirebaseAuthException catch (e) {
@@ -31,15 +30,31 @@ class FirebaseAuthMethods{
     required BuildContext context,
   }) async {
     try {
-      await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password
-      );
-      if(!_auth.currentUser!.emailVerified){
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      if (!_auth.currentUser!.emailVerified) {
         const Text('User Not found');
       }
-    }on FirebaseAuthException catch (e){
+    } on FirebaseAuthException catch (e) {
       showSnackBar(context, e.message!);
-
     }
   }
+
+  //EmailVerification
+  Future<void> sendEmailVerification(BuildContext context) async {
+    try {
+      _auth.currentUser!.sendEmailVerification();
+      showSnackBar(context, 'Email verification sent!');
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+    }
+  }
+
+  //Sign out
+  Future<void> signOut(BuildContext context) async {
+    try {
+      await _auth.signOut();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+    }
+  }
+}
