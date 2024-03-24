13a1551b0c2473ed1c68be6e0ddd68915b6400d7
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 15:14:39 +0530

    walk page

---
+++ lib/walk.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/signup_page.dart';
+
+class Walk extends StatelessWidget {
+  const Walk({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+         debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromRGBO(213, 202, 255, 1),
+      
