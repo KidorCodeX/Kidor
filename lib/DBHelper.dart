a8dc54cdb32a0b4e206363cd9476b7457d6f40fc
Amadee Ranasinghe <amadeeranasinghe@gmail.com>
2024-03-24 17:23:36 +0530

    Database helper created

---
+++ lib/DBHelper.dart
@@ -0,0 +1,14 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+class DBHelper {
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    // If _database is null, instantiate it
+    _database = await initDatabase();
+    return _database!;
+  }
