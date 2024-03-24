caeefe210393e031eeee2c18a44527d8875eae4a
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 17:13:55 +0530

    signup page button styles added

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -277,3 +277,40 @@ class _SignupState extends State<Signup> {
                           
                         }
                       },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                          EdgeInsets.zero,
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.transparent,
+                        ),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        width: 320,
+                        height: 60,
+                        decoration: ShapeDecoration(
+                          color: const Color.fromRGBO(107, 112, 248, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
