0951dca4b55b882359deea25025adc7d09413a3e
Devhan Dodampahala <devhandod@gmail.com>
2024-03-24 17:29:22 +0530

    change the ui

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -32,4 +32,95 @@ class QuizPage extends StatelessWidget {
             ),
           ],
         ),
-        
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 17),
+                  child: Container(
+                    width: 370,
+                    height: 125,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFF6855FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Container(
+                              width: 100,
+                              height: 90,
+                              decoration: ShapeDecoration(
+                                image: const DecorationImage(
+                                  image: AssetImage(
+                                      "assets/quizPage/quizBarPic.png"),
+                                  fit: BoxFit.fill,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.all(2.0),
+                                  child: SizedBox(
+                                    width: 200,
+                                    height: 30,
+                                    //color: Colors.black,
+                                    child: Text(
+                                      'Quiz | Revise',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(0),
+                                  child: Container(
+                                    width: 40,
+                                    height: 2,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 12),
+                                  child: SizedBox(
+                                    width: 260,
+                                    height: 40,
+                                    //color: Colors.black,
+                                    child: Text(
+                                      'Would you like to generate your own quiz for a different lesson? We got you!',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+               
