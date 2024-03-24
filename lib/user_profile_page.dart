36c90b68a3a83ab1875b45e10e3779ed80d53973
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 16:51:28 +0530

    User profile images added

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -71,3 +71,58 @@ class UserProfile extends StatelessWidget {
                   
                   ),
             ),
+            const SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: Center(
+                child: Text(
+                  "Student",
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 120,
+              //color: Colors.blue,
+              child: Center(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 166,
+                        height: 87,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.all(Radius.circular(10)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5), 
+                              spreadRadius: 5, 
+                              blurRadius: 7, 
+                              offset: const Offset(0, 3), 
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(13.0),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 40,
+                                height: 40,
+                                decoration: ShapeDecoration(
+                                image: const DecorationImage(
+                                  image: AssetImage("assets/Lessons.png"),
+                                  fit: BoxFit.fill,
+                                  ),
+                                
+                                  shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              ),
