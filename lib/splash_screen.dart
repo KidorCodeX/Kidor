d72ecec9c28d22be839b5e1923318b654f991566
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 16:45:07 +0530

    splash screen animations add

---
+++ lib/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:my_first_app/welcome_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+         debugShowCheckedModeBanner: false,
+      home: Stack(
+        children: [
+          Container(
+            color: Colors.purple, // Changed background color to purple
+          ),
+          AnimatedSplashScreen(
+            splash: Image.asset('assets/logo.jpg'),
+            nextScreen: const WelcomePage(),
+            splashTransition: SplashTransition.scaleTransition,
+            duration: 5000,
+          ),
+          
