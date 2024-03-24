66063aaf1b9925b5ce8f1a0727303bcbcaef1a36
Devhan Dodampahala <devhandod@gmail.com>
2024-03-24 17:26:17 +0530

    add the logo in quiz_page

---
+++ lib/quiz_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/QuizPage.dart';
+import 'package:my_first_app/chat_page.dart';
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            Container(
+              width: 100,
+              decoration: ShapeDecoration(
+                image: const DecorationImage(
+                  image: AssetImage("assets/logo.jpg"),
+                  fit: BoxFit.fill,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+            ),
+          ],
+        ),
+        
