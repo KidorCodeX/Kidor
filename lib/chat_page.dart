80e049d7bdd1d05daaefb17c82aec4a3d89e3661
Amadee Ranasinghe <amadeeranasinghe@gmail.com>
2024-03-24 17:27:21 +0530

    chat page created

---
+++ lib/chat_page.dart
@@ -0,0 +1,14 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:my_first_app/DBHelper.dart';
+import 'package:my_first_app/consts.dart';
+
+import 'dart:convert'; // Add this import for jsonDecode function
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
