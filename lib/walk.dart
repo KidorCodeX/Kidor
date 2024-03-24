444bda94a3f069decf37822d7c9055c7be59aba5
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 15:23:13 +0530

    walk three

--- lib/walk.dart
+++ lib/walk.dart
@@ -83,3 +83,134 @@ class Walk extends StatelessWidget {
                       ),
                     )
                   ),
+                  Center(
+                    child: Container(
+                      width: 70,
+                      height: 35,
+                      color: Colors.white,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 15,
+                                height: 15,
+                                decoration: ShapeDecoration(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  color: const Color.fromRGBO(107, 112, 248, 1),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 4),
+                                child: Container(
+                                  width: 10,
+                                  height: 10,
+                                  decoration: ShapeDecoration(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    color: const Color.fromRGBO(107, 112, 248, 1),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 4),
+                                child: Container(
+                                  width: 10,
+                                  height: 10,
+                                  decoration: ShapeDecoration(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    color: const Color.fromRGBO(107, 112, 248, 1),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                            child: Center(
+                              child: RawMaterialButton(
+                                onPressed: () {
+                                  // Add your onPressed logic here
+                                  print("Hello world");
+                                },
+                                child: const SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Center(
+                                    child: Text("Skip",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight:  FontWeight.w500,
+                                        color:  Color.fromRGBO(107, 112, 248, 1),
+                                      )
+                                    ),
+                                  )
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                            child: Center(
+                              child: RawMaterialButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => Walk2()),
+                                  );
+                                },
+                                elevation: 2.0,
+                                fillColor: const Color.fromRGBO(213, 202, 255, 1),
+                                shape: const CircleBorder(),
+                                padding: const EdgeInsets.all(20.0), 
+                                child: const SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.arrow_forward_rounded, 
+                                      color: Colors.white, 
+                                      size: 35, 
+                                    ),
+                                  )
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
