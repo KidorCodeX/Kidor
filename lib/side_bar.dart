3ca38b3a00898d79b8d094e9a45d2f85fa54306f
Devhan Dodampahala <devhandod@gmail.com>
2024-03-24 17:03:55 +0530

    side bar

---
+++ lib/side_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_first_app/QuizPage.dart';
+import 'package:my_first_app/ar_game_page.dart';
+import 'package:my_first_app/ar_portal_page.dart';
+import 'package:my_first_app/home_page.dart';
+import 'package:my_first_app/lessons_page.dart';
+import 'package:my_first_app/login_page.dart';
+import 'package:my_first_app/quiz_page.dart';
+import 'package:my_first_app/screens/home_screen.dart';
+import 'package:my_first_app/user_profile_page.dart';
+import 'package:my_first_app/views/home_screen.dart';
+import 'package:provider/provider.dart';
+
+
+
+class SideBar extends StatelessWidget {
+    const SideBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/sidebar.jpg'),
+                fit: BoxFit.fill,
+              ),
+       
+           
