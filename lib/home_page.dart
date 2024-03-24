b9d807a7204f7df0f1bec91379205b7607d35561
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-03-24 18:45:28 +0530

    Update function to handle edge cases

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:my_first_app/ar_game_page.dart';
+import 'package:my_first_app/ar_portal_page.dart';
+import 'package:my_first_app/lessons_page.dart';
+import 'package:my_first_app/quiz_page.dart';
+import 'package:my_first_app/screens/home_screen.dart';
+import 'package:my_first_app/side_bar.dart';
+import 'package:my_first_app/views/home_screen.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final String username;
+
+  const HomePage({Key? key, required this.username}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-            title: Container(
-          height: 50,
-          width: 90,
-          decoration: ShapeDecoration(
-            image: const DecorationImage(
-              image: AssetImage("assets/logo.jpg"),
-              fit: BoxFit.fill,
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(2),
-            ),
-          ),
-        )),
+            backgroundColor: Colors.white,
+            title: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                height: 50,
+                width: 90,
+                decoration: ShapeDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/logo.png"),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+              ),
+            )),
+        drawer: const SideBar(),
         body: SingleChildScrollView(
           child: Center(
             child: Column(
@@ -35,7 +51,7 @@ class HomePage extends StatelessWidget {
                       children: [
                         Center(
                           child: Padding(
-                            padding: const EdgeInsets.all(1.0),
+                            padding: const EdgeInsets.all(0.0),
                             child: Row(
                               children: [
                                 Padding(
@@ -45,23 +61,35 @@ class HomePage extends StatelessWidget {
                                     height: 135,
                                     decoration: const BoxDecoration(
                                         color: Colors.white),
-                                    child: const Column(
+                                    child: Column(
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
                                       children: [
-                                        Text(
-                                          'Welcome,',
-                                          style: TextStyle(
-                                            fontSize: 35,
+                                        Animate(
+                                          effects: [
+                                            FadeEffect(delay: 500.ms),
+                                            const SlideEffect(),
+                                          ],
+                                          child: const Text(
+                                            'Welcome,',
+                                            style: TextStyle(
+                                              fontSize: 35,
+                                            ),
                                           ),
                                         ),
-                                        Text(
-                                          'Tharusha !',
-                                          style: TextStyle(
-                                              fontSize: 27,
-                                              fontWeight: FontWeight.bold),
+                                        Animate(
+                                          effects: [
+                                            FadeEffect(delay: 500.ms),
+                                            const SlideEffect()
+                                          ],
+                                          child: Text(
+                                            '$username !',
+                                            style: const TextStyle(
+                                                fontSize: 27,
+                                                fontWeight: FontWeight.bold),
+                                          ),
                                         ),
-                                        Padding(
+                                        const Padding(
                                           padding: EdgeInsets.only(top: 17),
                                           child: Row(
                                             children: [
@@ -100,19 +128,27 @@ class HomePage extends StatelessWidget {
                                     ),
                                   ),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 20),
-                                  child: Container(
-                                    width: 105,
-                                    height: 105,
-                                    decoration: ShapeDecoration(
-                                      image: const DecorationImage(
-                                        image: AssetImage("assets/me.jpg"),
-                                        fit: BoxFit.fill,
-                                      ),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(100),
+                                SingleChildScrollView(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 0),
+                                    child: Animate(
+                                      effects: [
+                                        FadeEffect(delay: 500.ms),
+                                        const SlideEffect()
+                                      ],
+                                      child: Container(
+                                        width: 105,
+                                        height: 105,
+                                        decoration: ShapeDecoration(
+                                          image: const DecorationImage(
+                                            image: AssetImage("assets/me.jpg"),
+                                            fit: BoxFit.fill,
+                                          ),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -144,7 +180,8 @@ class HomePage extends StatelessWidget {
                             height: 100,
                             decoration: ShapeDecoration(
                               image: const DecorationImage(
-                                image: AssetImage("assets/astronaut.png"),
+                                image:
+                                    AssetImage("assets/homePage/astronaut.png"),
                                 fit: BoxFit.fill,
                               ),
                               shape: RoundedRectangleBorder(
@@ -250,7 +287,7 @@ class HomePage extends StatelessWidget {
                     margin: const EdgeInsets.all(10),
                     child: Center(
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 17),
+                        padding: const EdgeInsets.only(left: 13),
                         child: Row(
                           children: [
                             Container(
@@ -258,34 +295,46 @@ class HomePage extends StatelessWidget {
                               height: 155,
                               decoration: BoxDecoration(
                                 image: const DecorationImage(
-                                  image: AssetImage("assets/handwork.jpg"),
+                                  image: AssetImage(
+                                      "assets/homePage/lessonQuiz.png"),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.circular(15.0),
                               ),
                               child: RawMaterialButton(
                                 onPressed: () {
-                                  // Add your onPressed logic here
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const QuizHomePage()),
+                                  );
                                 },
                                 shape: const RoundedRectangleBorder(),
                                 padding: const EdgeInsets.all(20.0),
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 30),
+                              padding: const EdgeInsets.only(left: 22),
                               child: Container(
                                 width: 163,
                                 height: 155,
                                 decoration: BoxDecoration(
                                   image: const DecorationImage(
-                                    image: AssetImage("assets/arLessons.jpg"),
+                                    image: AssetImage(
+                                        "assets/homePage/arLessons.jpg"),
                                     fit: BoxFit.cover,
                                   ),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 child: RawMaterialButton(
                                   onPressed: () {
-                                    // Add your onPressed logic here
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const LessonsPage()),
+                                    );
                                   },
                                   shape: const RoundedRectangleBorder(),
                                   padding: const EdgeInsets.all(20.0),
@@ -303,7 +352,7 @@ class HomePage extends StatelessWidget {
                     margin: const EdgeInsets.all(10),
                     child: Center(
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 17),
+                        padding: const EdgeInsets.only(left: 13),
                         child: Row(
                           children: [
                             Container(
@@ -311,34 +360,45 @@ class HomePage extends StatelessWidget {
                               height: 155,
                               decoration: BoxDecoration(
                                 image: const DecorationImage(
-                                  image: AssetImage("assets/arGames.jpg"),
+                                  image:
+                                      AssetImage("assets/homePage/arGames.jpg"),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.circular(15.0),
                               ),
                               child: RawMaterialButton(
                                 onPressed: () {
-                                  // Add your onPressed logic here
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const GamePage()),
+                                  );
                                 },
                                 shape: const RoundedRectangleBorder(),
                                 padding: const EdgeInsets.all(20.0),
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 30),
+                              padding: const EdgeInsets.only(left: 22),
                               child: Container(
                                 width: 163,
                                 height: 155,
                                 decoration: BoxDecoration(
                                   image: const DecorationImage(
-                                    image: AssetImage("assets/arPortal.jpg"),
+                                    image: AssetImage(
+                                        "assets/homePage/arPortal.jpg"),
                                     fit: BoxFit.cover,
                                   ),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 child: RawMaterialButton(
                                   onPressed: () {
-                                    // Add your onPressed logic here
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const ArPortalPage()),
+                                    );
                                   },
                                   shape: const RoundedRectangleBorder(),
                                   padding: const EdgeInsets.all(20.0),
@@ -356,7 +416,7 @@ class HomePage extends StatelessWidget {
                     margin: const EdgeInsets.all(10),
                     child: Center(
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 17),
+                        padding: const EdgeInsets.only(left: 13),
                         child: Row(
                           children: [
                             Container(
@@ -364,34 +424,45 @@ class HomePage extends StatelessWidget {
                               height: 155,
                               decoration: BoxDecoration(
                                 image: const DecorationImage(
-                                  image: AssetImage("assets/howArWorks.jpg"),
+                                  image: AssetImage(
+                                      "assets/homePage/noteBook.png"),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.circular(15.0),
                               ),
                               child: RawMaterialButton(
                                 onPressed: () {
-                                  // Add your onPressed logic here
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const NoteBook()),
+                                  );
                                 },
                                 shape: const RoundedRectangleBorder(),
                                 padding: const EdgeInsets.all(20.0),
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 30),
+                              padding: const EdgeInsets.only(left: 22),
                               child: Container(
                                 width: 163,
                                 height: 155,
                                 decoration: BoxDecoration(
                                   image: const DecorationImage(
-                                    image: AssetImage("assets/quizzesMain.jpg"),
+                                    image: AssetImage(
+                                        "assets/homePage/quizzesMain.jpg"),
                                     fit: BoxFit.cover,
                                   ),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 child: RawMaterialButton(
                                   onPressed: () {
-                                    // Add your onPressed logic here
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const QuizPage()),
+                                    );
                                   },
                                   shape: const RoundedRectangleBorder(),
                                   padding: const EdgeInsets.all(20.0),
