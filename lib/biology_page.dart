ec055e0c04624f49e91c3854e0258ef78dfb3461
thisarihk <hkthisari@gmail.com>
2024-03-24 17:45:59 +0530

    Added styles

--- lib/biology_page.dart
+++ lib/biology_page.dart
@@ -71,3 +71,125 @@ class BiologyPage extends StatelessWidget {
     }
   }
 }
+
+class LessonCard extends StatelessWidget {
+  final String lessonImagePath;
+  final String heading;
+  final String paragraph;
+  final String gameButtonImagePath;
+  final VoidCallback onTap;
+
+  const LessonCard({
+    Key? key,
+    required this.lessonImagePath,
+    required this.heading,
+    required this.paragraph,
+    required this.gameButtonImagePath,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: Container(
+        width: 350,
+        height: 150,
+        child: Material(
+          elevation: 4,
+          borderRadius: BorderRadius.circular(15),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  width: 130,
+                  height: 120,
+                  decoration: ShapeDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(lessonImagePath),
+                      fit: BoxFit.fill,
+                    ),
+                    color: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 1),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 27, right: 64, bottom: 5),
+                      child: Container(
+                        width: 104,
+                        height: 25,
+                        child: Text(
+                          heading,
+                          style: const TextStyle(
+                            fontSize: 22.5,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: SizedBox(
+                        width: 168,
+                        height: 40,
+                        child: Text(
+                          paragraph,
+                          style: const TextStyle(
+                            fontSize: 13,
+                            height: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: onTap,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 50),
+                            child: Container(
+                              width: 77,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(gameButtonImagePath),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 12),
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("assets/heartL.png"),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
