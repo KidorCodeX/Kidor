2422dbbd8fb54c45cf72fcb156f8e15bc783bc64
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 16:50:27 +0530

    User profile body added

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -28,4 +28,46 @@ class UserProfile extends StatelessWidget {
             ),
           ],
         ),
-      
+      body: 
+        Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 208,
+              child: Center(
+            child: Stack(
+                  children: [
+                    Positioned(
+                      left: 30,
+                      top: 20,
+                      child: Container(
+                        width: 350,
+                        height: 115,
+                        decoration: const BoxDecoration(color: Color(0xFFD4C9FE)),
+                      ),
+                      
+                    ),
+                    
+                    Positioned(
+                      left: 148,
+                      top: 71,
+                      child: Container(
+                        width: 115,
+                        height: 118,
+                        decoration: ShapeDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage('assets/me.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                      ),
+                    ),
+            
+                  ],
+                ),
+                  
+                  ),
+            ),
