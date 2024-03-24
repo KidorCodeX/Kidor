60409d669bdd4fea7f7b5d5b105109e1adcbb2c6
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 20:22:18 +0530

    git hub automations added

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "App bar",
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),
