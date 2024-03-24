12378d37ee4b74504868c838ba72c25e5f10470f
Devhan Dodampahala <devhandod@gmail.com>
2024-03-24 16:08:52 +0530

    comment

--- lib/consts.dart
+++
@@ -1,5 +0,0 @@
-const OPENAI_API = "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";
-
-//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
-
-//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
