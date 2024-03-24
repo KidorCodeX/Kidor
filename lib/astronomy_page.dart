f3cdd91e55f9dd2140f4af023870801536b0afac
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-03-24 22:17:35 +0530

    Lessons pages changes

--- lib/astronomy_page.dart
+++ lib/astronomy_page.dart
@@ -3,11 +3,9 @@ import 'package:my_first_app/web_view_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final Uri _url = Uri.parse('https://webxr.run/4X5EpkPnbxeoG');
-final Uri _url1 = Uri.parse('https://ar-code.com/ec6gEkIqh');
+final Uri _url = Uri.parse('https://webxr.run/k7rWmZx2R8J7x');
+final Uri _url1 = Uri.parse('https://webxr.run/Y9JrVynJ4Myyx');
 final Uri _url2 = Uri.parse('https://webxr.run/4X5EpkPnbxeoG');
-
-
 
 class Astronomy extends StatelessWidget {
   const Astronomy({Key? key}) : super(key: key);
@@ -15,79 +13,73 @@ class Astronomy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-         debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 170,
-              decoration: const BoxDecoration(
+          body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 170,
+            decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage("assets/astronomyPage/astronomyBarPic.png"),
                   fit: BoxFit.fill,
                 ),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(150)
-                )
-              ),
-            ),
-            LessonCard(
-              lessonImagePath: "assets/astronomyPage/layer.png",
-              heading: "Earth's layers",
-              paragraph: "Learn about the layers in the planet earth ",
-              gameButtonImagePath: "assets/gameButton.png",
-              onTap:
-              _launchUrl,
-            ),
-            LessonCard(
-              lessonImagePath: "assets/astronomyPage/solar.png",
-              heading: "Solar System",
-              paragraph: "Learn about the layers in the planet earth Solar System",
-              gameButtonImagePath: "assets/gameButton.png",
-               onTap:_launchUrl1,
-            ),
-            LessonCard(
-              lessonImagePath: "assets/astronomyPage/star.png",
-              heading: "Star Patterns",
-              paragraph: "Learn everything you want to know about star patterns",
-              gameButtonImagePath: "assets/gameButton.png",
-              onTap:_launchUrl2,
-            ),
-
-            
-          ],
-        )
-
-
-      ),
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(150))),
+          ),
+          LessonCard(
+            lessonImagePath: "assets/astronomyPage/layer.png",
+            heading: "Earth's layers",
+            paragraph: "Learn about the layers in the planet earth ",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl,
+          ),
+          LessonCard(
+            lessonImagePath: "assets/astronomyPage/solar.png",
+            heading: "Solar System",
+            paragraph:
+                "Learn about the layers in the planet earth Solar System",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl1,
+          ),
+          LessonCard(
+            lessonImagePath: "assets/astronomyPage/star.png",
+            heading: "Star Patterns",
+            paragraph: "Learn everything you want to know about star patterns",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl2,
+          ),
+        ],
+      )),
     );
   }
+
   Future<void> _launchUrl() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
-  }
+
   Future<void> _launchUrl1() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
+    if (!await launchUrl(_url1)) {
+      throw Exception('Could not launch $_url1');
+    }
   }
-  }
+
   Future<void> _launchUrl2() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
-  }
+    if (!await launchUrl(_url2)) {
+      throw Exception('Could not launch $_url2');
+    }
   }
 }
-
 
 class LessonCard extends StatelessWidget {
   final String lessonImagePath;
   final String heading;
   final String paragraph;
-  final String gameButtonImagePath; 
-  final VoidCallback onTap; 
+  final String gameButtonImagePath;
+  final VoidCallback onTap;
 
   const LessonCard({
     Key? key,
@@ -132,7 +124,8 @@ class LessonCard extends StatelessWidget {
                 child: Column(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(top: 27, right: 40, bottom: 5),
+                      padding:
+                          const EdgeInsets.only(top: 27, right: 40, bottom: 5),
                       child: Container(
                         width: 140,
                         height: 28,
@@ -209,6 +202,3 @@ void _launchURL(BuildContext context, String url) {
     MaterialPageRoute(builder: (context) => WebViewPage(url: url)),
   );
 }
-
-
-
--- lib/maths_page.dart
+++ lib/maths_page.dart
@@ -4,10 +4,8 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 final Uri _url = Uri.parse('https://webxr.run/4X5EpkPnbxeoG');
-final Uri _url1 = Uri.parse('https://ar-code.com/ec6gEkIqh');
-final Uri _url2 = Uri.parse('https://webxr.run/4X5EpkPnbxeoG');
-
-
+final Uri _url1 = Uri.parse('https://webxr.run/Zn16exWgve2ll');
+final Uri _url2 = Uri.parse('https://webxr.run/XV8P8eaDAZlNo');
 
 class MathsPage extends StatelessWidget {
   const MathsPage({Key? key}) : super(key: key);
@@ -15,80 +13,72 @@ class MathsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-         debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 170,
-              decoration: const BoxDecoration(
+          body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 170,
+            decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage("assets/mathsPage/mathsBar.png"),
                   fit: BoxFit.fill,
                 ),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(150)
-                )
-              ),
-            ),
-            LessonCard(
-              lessonImagePath: "assets/mathsPage/circleL.png",
-              heading: "Circle",
-              paragraph: "Learn everything  about circles and its formulas ",
-              gameButtonImagePath: "assets/gameButton.png",
-              onTap:
-              _launchUrl,
-            ),
-            LessonCard(
-              lessonImagePath: "assets/mathsPage/triangleL.png",
-              heading: "Triangle",
-              paragraph: "Learn everything  about triangles and its formulas",
-              gameButtonImagePath: "assets/gameButton.png",
-              onTap:_launchUrl1,
-            ),
-            LessonCard(
-              lessonImagePath: "assets/mathsPage/cubeL.png",
-              heading: "Cube",
-              paragraph: "Learn everything  about cubes and its formulas ",
-              gameButtonImagePath: "assets/gameButton.png",
-              onTap:_launchUrl2,
-            ),
-
-            
-          ],
-        )
-
-
-      ),
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(150))),
+          ),
+          LessonCard(
+            lessonImagePath: "assets/mathsPage/circleL.png",
+            heading: "Circle",
+            paragraph: "Learn everything  about circles and its formulas ",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl,
+          ),
+          LessonCard(
+            lessonImagePath: "assets/mathsPage/triangleL.png",
+            heading: "Pyramid",
+            paragraph: "Learn everything  about Pyramids and its formulas",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl1,
+          ),
+          LessonCard(
+            lessonImagePath: "assets/mathsPage/cubeL.png",
+            heading: "Cube",
+            paragraph: "Learn everything  about cubes and its formulas ",
+            gameButtonImagePath: "assets/gameButton.png",
+            onTap: _launchUrl2,
+          ),
+        ],
+      )),
     );
   }
 
   Future<void> _launchUrl() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
-  }
+
   Future<void> _launchUrl1() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
+    if (!await launchUrl(_url1)) {
+      throw Exception('Could not launch $_url1');
+    }
   }
-  }
+
   Future<void> _launchUrl2() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
-  }
+    if (!await launchUrl(_url2)) {
+      throw Exception('Could not launch $_url2');
+    }
   }
 }
-
 
 class LessonCard extends StatelessWidget {
   final String lessonImagePath;
   final String heading;
   final String paragraph;
-  final String gameButtonImagePath; 
-  final VoidCallback onTap; 
+  final String gameButtonImagePath;
+  final VoidCallback onTap;
 
   const LessonCard({
     Key? key,
@@ -133,7 +123,8 @@ class LessonCard extends StatelessWidget {
                 child: Column(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(top: 27, right: 60, bottom: 5),
+                      padding:
+                          const EdgeInsets.only(top: 27, right: 60, bottom: 5),
                       child: Container(
                         width: 115,
                         height: 25,
@@ -210,6 +201,3 @@ void _launchURL(BuildContext context, String url) {
     MaterialPageRoute(builder: (context) => WebViewPage(url: url)),
   );
 }
-
-
-
