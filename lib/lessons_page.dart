defd43e15e6eb7df5c3230861d7b9b023341fc8e
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-03-24 17:52:08 +0530

    Add AR Learning Gateway widget and Subjects section

--- lib/lessons_page.dart
+++ lib/lessons_page.dart
@@ -34,6 +34,7 @@ class LessonsPage extends StatelessWidget {
           ],
         ),
 
+
       body: SingleChildScrollView( 
         child: Center(
           child: Column(
@@ -47,3 +48,108 @@ class LessonsPage extends StatelessWidget {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15),
                   ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          width: 100,
+                          height: 90,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage("assets/lessonsPage/binocular.png"),
+                              fit: BoxFit.fill,
+                              ),
+                                        
+                              shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(2.0),
+                              child: SizedBox(
+                                width: 200,
+                                height: 30,
+                                //color: Colors.black,
+                                child: Text(
+                                  'AR Learning Gateway',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: Container(
+                                width: 40,
+                                height: 2,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 12),
+                              child: SizedBox(
+                                width: 260,
+                                height: 40,
+                                //color: Colors.black,
+                                child: Text(
+                                  'Enjoy learning with the newest technology...',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  color: Colors.white,
+                  margin: const EdgeInsets.all(10),
+                  child: const Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.local_fire_department,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              'Subjects',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Icon(
+                          Icons.filter_list_outlined,                  
