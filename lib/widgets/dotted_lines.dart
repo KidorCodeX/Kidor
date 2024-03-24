00c282dc3c2b8dc7b96bf3449d5962889b2b6918
Amadee Ranasinghe <amadeeranasinghe@gmail.com>
2024-03-24 17:56:36 +0530

    final update

--- lib/widgets/dotted_lines.dart
+++ lib/widgets/dotted_lines.dart
@@ -11,6 +11,7 @@ class DrawDottedhorizontalline extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     for (double i = -300; i < 300; i = i + 15) {
+      //for loop
       // 15 is space between dots
       if (i % 3 == 0) {
         canvas.drawLine(Offset(i, 0.0), Offset(i + 10, 0.0), _paint);
